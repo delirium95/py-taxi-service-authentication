c5e5270750dbc845dd5bd73180f3100888397547
Nikita Grinyov <nikita3grynov@gmail.com>
2025-12-13 23:10:58 +0200

    task

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 from django.urls import reverse
 
 
-def login(request: HttpRequest) -> HttpResponse:
+def login_view(request: HttpRequest) -> HttpResponse:
     if request.method == "GET":
-        return render(request, "accounts/login.html")
+        return render(request, "registration/login.html")
     elif request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -19,10 +19,10 @@ def login(request: HttpRequest) -> HttpResponse:
                 "error": "Invalid credentials"
             }
             return render(request,
-                          "accounts/login.html",
+                          "registration/login.html",
                           context=error_context)
 
 
-def logout(request: HttpRequest) -> HttpResponse:
+def logout_view(request: HttpRequest) -> HttpResponse:
     logout(request)
-    return render(request, "accounts/logged_out.html")
+    return render(request, "registration/logged_out.html")
