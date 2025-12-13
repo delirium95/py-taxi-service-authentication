079453c2dc2531bad9976e0ff96b364d7b68ac69
Nikita Grinyov <nikita3grynov@gmail.com>
2025-12-13 23:32:14 +0200

    task

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
 from django.shortcuts import render
@@ -6,14 +7,15 @@ from django.views import generic
 from .models import Driver, Car, Manufacturer
 
 
+@login_required
 def index(request: HttpRequest):
     """View function for the home page of the site."""
 
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
+    num_visits = request.session.get("num_visits", 0) + 1
+    request.session["num_visits"] = num_visits
 
     context = {
         "num_drivers": num_drivers,
